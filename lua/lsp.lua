e88924c132eae705f8a578a440de40d9c8946148
snesjhon <snesjhon@gmail.com>
2026-08-24 16:02:24 -0700

    feat: update mappings and blink

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,16 +2,8 @@ vim.lsp.config("vtsls", {
   cmd = { "vtsls", "--stdio" },
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+  capabilities = require("blink.cmp").get_lsp_capabilities(),
 })
 vim.lsp.enable("vtsls")
 
 vim.diagnostic.config({ virtual_text = { spacing = 2, prefix = "●" } })
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
-    end
-  end,
-})
